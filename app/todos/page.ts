b5ef32c92dc8f02b1f09b9c086c99cb60765cb0a
やすゆ〜き <desrow.9999@gmail.com>
2026-09-07 23:17:36 +0000

    todosのコードを修正

--- app/todos/page.ts
+++ app/todos/page.ts
@@ -1,17 +1,17 @@
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server';
 
-export default async function Page() {
-  const supabase = await createClient()
-  const { data: todos } = await supabase.from('todos').select()
+export default async function TodosPage() {
+  const supabase = await createClient();
+  const { data: todos } = await supabase.from('todos').select('*');
 
   return (
     <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Todoリスト</h1>
+      <h1 className="text-2xl font-bold mb-4">記録一覧</h1>
       <ul className="list-disc pl-5">
         {todos?.map((todo: any) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
     </main>
-  )
+  );
 }
