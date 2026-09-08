6822abaf7c1d31643319187a8f8531c2a3d15396
やすゆ〜き <desrow.9999@gmail.com>
2026-09-08 00:14:09 +0000

    client.ts側もSupabase設定を直接記述

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    'https://mwurdtuqkgnqplaqscrg.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13dXJkdHVxa2ducXBsYXFzY3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NTYyMTIsImV4cCI6MjEwNDEzMjIxMn0.JpcU343VYpelgIXB4V589KOc8M1ENGcWu7tVoqsUlMA'
   )
 }
